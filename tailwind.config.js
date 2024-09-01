5853f6147ebe3a76e06262808fdd6c9e7b9869a0
Sharif <mdsh8692@gmail.com>
2024-09-01 21:40:53 +0600

    navbar for desktop done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['*'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-heading': 'rgb(33, 33, 33)',
+        'primary-bg': 'rgb(171, 239, 95)',
+        'border-color': 'rgb(233, 233, 233)',
+        'button-bg': '#ABEF5F',
+        
+      },
+      backgroundImage: {
+        'full-body': 'linear-gradient(0.00deg, rgb(255, 255, 255),rgba(255, 255, 255, 0) 100%)',
+      }
+    },
   },
   plugins: [],
 }
